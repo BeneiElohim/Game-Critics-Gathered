b9b4ce4200c762a5e9725e44f7c3da3c7596dd2f
ismailtarihc <0105mert@gmail.com>
2023-03-02 01:08:10 +0300

    random content push to the boxes

--- js/content.js
+++ js/content.js
@@ -1,25 +1,28 @@
 let contentBoxes = document.getElementsByClassName("content-box");
 let randomIndex;
-let databaseContent;
 
-indexDB.forEach((content) => {
-    if (content.usedContent === false) {
-        databaseContent.push(content);
-    }
-});
+function loadContent() {
 Array.from(contentBoxes).forEach(box => {
     randomIndex = Math.floor(Math.random() * 10);
     try {
-        if (databaseContent[randomIndex].usedContent === false) {
-            box.querySelector(".title").textContent = databaseContent[randomIndex].title;
-            box.querySelector(".description").textContent = databaseContent[randomIndex].description;
-            box.querySelector("img").src = databaseContent[randomIndex].image;
-            box.querySelector(".link").href = databaseContent[randomIndex].url;
-            databaseContent[randomIndex].usedContent = true;
+        if (indexDB[randomIndex].usedContent == false) {
+            console.log("used content");
+            box.querySelector(".title").textContent = indexDB[randomIndex].title;
+            box.querySelector(".description").textContent = indexDB[randomIndex].description;
+            box.querySelector("img").src = indexDB[randomIndex].image;
+            box.querySelector(".link").href = indexDB[randomIndex].url;
+            indexDB[randomIndex].usedContent = true;
         } else {
-            randomIndex = Math.floor(Math.random() * databaseContent.length);
+            randomIndex = Math.floor(Math.random() * indexDB.length);
         }
     } catch (error) {
         console.log(error);
     }
 });
+}
+//wait for the indexDB to be populated, then run the loadContent function with async/await
+async function wait() {
+await new Promise((resolve) => setTimeout(resolve, 1000));
+loadContent();
+}
+wait();
