7bc348c82a8a568216e6ab272e69cba0cafca21e
ismailtarihc <0105mert@gmail.com>
2023-03-10 02:58:02 +0300

    main page styling

--- js/contents/post1.js
+++
@@ -1,6 +0,0 @@
-const input = {
-    postTitle: "Post 1",
-}
-const template = Handlebars.compile(document.querySelector('#post-template').innerHTML);
-const html = template({ input });
-document.getElementById("titleOfPost").innerHTML = html;
