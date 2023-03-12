5d3dfaf5cf5684cca32b8f905a1224db54a09383
ismailtarihc <0105mert@gmail.com>
2023-03-12 05:36:03 +0300

    scroll to top button

---
+++ js/scrollbutton.js
@@ -0,0 +1,14 @@
+let scrollButton = document.getElementById("scrollbutton");
+window.onscroll = function() {scrollFunction()};
+function scrollFunction() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+      scrollButton.style.display = "block";
+    } else {
+      scrollButton.style.display = "none";
+    }
+}
+// When the user clicks on the button, scroll to the top of the document
+function scrollToTop() {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+}
